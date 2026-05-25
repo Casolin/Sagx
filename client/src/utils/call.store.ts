005bbb258afbc94febe8b4c087bccfa40f60313f
Casolin <animecaster@gmail.com>
2026-05-25 14:02:06 +0100

    xqc

--- client/src/utils/call.store.ts
+++ client/src/utils/call.store.ts
@@ -243,6 +243,13 @@ export const useCallStore = create<CallState>((set, get) => ({
 
     if (!peer || !stream) return;
 
+    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isMobile && !navigator.mediaDevices?.getDisplayMedia) {
+      alert("Screen sharing is not supported on this mobile browser");
+      return;
+    }
+
     // eslint-disable-next-line
     const pc = (peer as any)._pc as RTCPeerConnection;
 
