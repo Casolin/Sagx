305457bdc5d8a6df84b7bc5b3bf66d299565bf2d
Casolin <animecaster@gmail.com>
2026-05-26 16:50:31 +0100

    xqc

--- client/src/components/CallBusyModal.tsx
+++ client/src/components/CallBusyModal.tsx
@@ -10,7 +10,7 @@ export const CallBusyModal = ({
   return (
     <Dialog.Root open={open} onOpenChange={onClose}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+        <Dialog.Overlay className="fixed inset-0 z-50 flex items-center justify-center bg-black/50" />
 
         <Dialog.Content className="fixed left-1/2 top-1/2 w-[320px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-white p-6 shadow-xl">
           <Dialog.Title className="text-lg font-semibold">
