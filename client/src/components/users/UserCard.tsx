260785be839a874e40d596873b739d522a54cca6
Casolin <animecaster@gmail.com>
2026-05-30 14:54:50 +0100

    xqc

--- client/src/components/users/UserCard.tsx
+++ client/src/components/users/UserCard.tsx
@@ -66,15 +66,6 @@ export default function UserCard({ user, onEdit, onDelete }: Props) {
               src={user.avatar}
               className="w-12 h-12 rounded-full border object-cover"
             />
-            <span
-              className={`absolute -bottom-1 -right-1 w-3 h-3 rounded-full border-2 border-white ${
-                user.status === "ACTIVE"
-                  ? "bg-green-500"
-                  : user.status === "SUSPENDED"
-                  ? "bg-red-500"
-                  : "bg-gray-400"
-              }`}
-            />
           </div>
 
           {/* info */}
@@ -121,7 +112,7 @@ export default function UserCard({ user, onEdit, onDelete }: Props) {
             onClick={() => setOpen(true)}
             className="
             px-3 py-2 rounded-xl text-sm font-medium
-            bg-red-600 text-black
+            bg-red-600 text-white
             hover:bg-red-100 transition
             cursor-pointer
           "
